ff481ce8c0f9c168ff0c76c447ddadc445bc2e63
Daryna Parfeniuk <darina.parfeniuk@gmail.com>
2020-09-30 14:03:51 +0300

    Comment

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
-//mobile button catalog show/hide
+// mobile button catalog show/hide
 const buttonToCatalog = document.querySelector(".header__button-catalog");
 const mobileCatalog = document.querySelector(".header__catalog__mobile");
 
 buttonToCatalog.addEventListener("click", function() {
     if (mobileCatalog.style.display === "none") {
         mobileCatalog.style.display = "grid";
+
     } else {
         mobileCatalog.style.display = "none";
+    }
+});
+
+// mobile menu (hamburger)
+const buttonHamburger = document.querySelector(".header__navigation__menu");
+const mobileNavigation = document.querySelector(".header__navigation__mobile");
+
+buttonHamburger.addEventListener("click", function() {
+    if (mobileNavigation.style.display === "none") {
+        mobileNavigation.style.display = "block";
+    } else {
+        mobileNavigation.style.display = "none";
     }
 });
