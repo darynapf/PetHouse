e69cee52c82b06774d4442e7d06b8a7ed056e397
Daryna Parfeniuk <darina.parfeniuk@gmail.com>
2020-10-06 16:57:52 +0300

    Comment

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ const buttonToCatalog = document.querySelector(".header__button-catalog");
 const mobileCatalog = document.querySelector(".header__catalog__mobile");
 
 buttonToCatalog.addEventListener("click", function() {
-    if (mobileCatalog.style.display === "none") {
+    if (!mobileCatalog.style.display || mobileCatalog.style.display === "none") {
         mobileCatalog.style.display = "grid";
     } else {
         mobileCatalog.style.display = "none";
